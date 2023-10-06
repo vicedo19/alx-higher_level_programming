1ab0a923296ff829d89a4e7f62767c9112e692f0
Vicedo19 <victorugochukwuka@gmail.com>
2023-10-06 11:48:54 -0700

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    from calculator_1 import *
+    from calculator_1 import add, sub, mul, div
     count = len(argv)
 
     if count != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    operator = argv[2]
+
+    def not_found():
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+        exit()
+
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+        exit()
+
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+        exit()
+
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+        exit()
+
+    else:
+        print(not_found())
