a873d80079b73cf8cc174b7f7d7753ed188f3c21
Vicedo19 <victorugochukwuka@gmail.com>
2023-10-07 18:39:17 -0700

    concatenate edges updated

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[str.find("object"):str.find("language") - 1]) + " " + (str[str.find("with"):str.find("very")]) + str[:6]
+str = (str[str.find("object"):str.find("language")]) + (str[str.find("with"):str.find("very")]) + str[:6]
 print(str)
