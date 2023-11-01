e028f2ca645afb4059f04f816a314f196d61f8bf
Vicedo19 <victorugochukwuka@gmail.com>
2023-10-31 21:41:23 -0700

    python objects project

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+        super().__setattr__(name, value)
