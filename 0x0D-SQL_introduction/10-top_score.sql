89ca90f2d972e08d16ece1fd576974fcf681f793
vicedo19 <victorugochukwuka@gmail.com>
2024-02-13 17:32:10 -0800

    MySQL Projects completed

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
--- Lists all records of the table second_table.
--- Records are ordered by descending score.
+-- Lists all records of the table second_table, ordered by score in descending order.
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
